98894ff0807b4aa33516203e6666f58ac6153ab9
MialyR <rakotozanany.mi@gmail.com>
2021-09-18 13:02:23 +0300

    training card + equipment design

--- app/src/main/java/com/example/hallinonesport/view/home/training/TrainingAdapter.java
+++ app/src/main/java/com/example/hallinonesport/view/home/training/TrainingAdapter.java
@@ -8,9 +8,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.hallinonesport.R;
-import com.example.hallinonesport.model.Equipment;
 import com.example.hallinonesport.model.Training;
-import com.example.hallinonesport.view.home.equipment.CardEquipmentHolder;
 
 import java.util.List;
 
@@ -26,7 +24,7 @@ public class TrainingAdapter extends RecyclerView.Adapter<TrainingHolder> {
     @NonNull
     @Override
     public TrainingHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_training, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_training, parent, false);
         return new TrainingHolder(view);
     }
 
