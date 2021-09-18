f49b6bc3978f992452fdb1c37de1b44fd2743a1e
MialyR <rakotozanany.mi@gmail.com>
2021-09-18 14:05:52 +0300

    add menu

--- app/src/main/java/com/example/hallinonesport/view/home/equipment/EquipmentFragment.java
+++ app/src/main/java/com/example/hallinonesport/view/home/equipment/EquipmentFragment.java
@@ -2,11 +2,15 @@ package com.example.hallinonesport.view.home.equipment;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -77,6 +81,7 @@ public class EquipmentFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_equipment, container, false);
+        setHasOptionsMenu(true);
         this.recyclerView = view.findViewById(R.id.recycler_view);
         this.adapter = new EquipmentAdapter(getListEquipment());
         FlexboxLayoutManager manager = new FlexboxLayoutManager(view.getContext());
@@ -88,6 +93,16 @@ public class EquipmentFragment extends Fragment {
         this.recyclerView.setAdapter(this.adapter);
 
         return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setQueryHint("Recherche");
+
+        super.onCreateOptionsMenu(menu,inflater);
     }
 
     private List<Equipment> getListEquipment() {
