171684e5439d06659738f6bfc0872291b30209c6
MialyR <rakotozanany.mi@gmail.com>
2021-09-20 01:01:15 +0300

    display preference and account

--- app/src/main/java/com/example/hallinonesport/view/account/AccountFragment.java
+++ app/src/main/java/com/example/hallinonesport/view/account/AccountFragment.java
@@ -2,11 +2,16 @@ package com.example.hallinonesport.view.account;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 import com.example.hallinonesport.R;
 
@@ -25,6 +30,17 @@ public class AccountFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private LinearLayout background;
+
+    private CardView preferenceCard;
+    private LinearLayout preference;
+    private Button savePreference;
+    private ImageButton closePreference;
+
+    private CardView accountCard;
+    private LinearLayout accountModal;
+    private ImageButton closeAccount;
 
     public AccountFragment() {
         // Required empty public constructor
@@ -60,7 +76,59 @@ public class AccountFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        View view = inflater.inflate(R.layout.fragment_account, container, false);
+
+        this.preferenceCard = view.findViewById(R.id.preferenceCard);
+        this.preference = view.findViewById(R.id.preferenceModal);
+        this.background = view.findViewById(R.id.overbox);
+        this.savePreference = view.findViewById(R.id.savePreference);
+        this.closePreference = view.findViewById(R.id.closePreference);
+
+        this.preferenceCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                preference.setVisibility(View.VISIBLE);
+                background.setVisibility(View.VISIBLE);
+            }
+        });
+
+        this.savePreference.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                preference.setVisibility(View.INVISIBLE);
+                background.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        this.closePreference.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                preference.setVisibility(View.INVISIBLE);
+                background.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        this.accountCard = view.findViewById(R.id.accountCard);
+        this.accountModal = view.findViewById(R.id.accountModal);
+        this.closeAccount = view.findViewById(R.id.closeAccount);
+
+        this.accountCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                accountModal.setVisibility(View.VISIBLE);
+                background.setVisibility(View.VISIBLE);
+            }
+        });
+
+        this.closeAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                accountModal.setVisibility(View.INVISIBLE);
+                background.setVisibility(View.INVISIBLE);
+            }
+        });
+
+
+        return view;
     }
 }
