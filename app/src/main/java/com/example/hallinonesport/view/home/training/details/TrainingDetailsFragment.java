954ea88a81e0d32775b5f6d1a0e17f30226ea363
Anjara <rmanjara@gmail.com>
2021-09-21 20:30:52 +0100

    change icon

--- app/src/main/java/com/example/hallinonesport/view/home/training/details/TrainingDetailsFragment.java
+++ app/src/main/java/com/example/hallinonesport/view/home/training/details/TrainingDetailsFragment.java
@@ -4,18 +4,22 @@ import android.annotation.SuppressLint;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.asura.library.posters.DrawableImage;
 import com.asura.library.posters.Poster;
@@ -30,6 +34,8 @@ import com.example.hallinonesport.tools.Useful;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 public class TrainingDetailsFragment extends Fragment {
 
@@ -37,9 +43,11 @@ public class TrainingDetailsFragment extends Fragment {
     private Button startButton;
     private TextView title;
     private TextView description;
+    private TextView timerText;
     private Training training;
     private TrainingController controller;
     private SettingController  settingController;
+    private boolean start = false;
 
     public TrainingDetailsFragment(Training training) {
         this.settingController = SettingController.getInstance(getContext());
@@ -52,6 +60,7 @@ public class TrainingDetailsFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @SuppressLint("ResourceAsColor")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -62,6 +71,9 @@ public class TrainingDetailsFragment extends Fragment {
         this.startButton = (Button) view.findViewById(R.id.startButton);
         this.title = (TextView) view.findViewById(R.id.training_details_title);
         this.description = (TextView) view.findViewById(R.id.training_details_description);
+        this.timerText = (TextView) view.findViewById(R.id.training_details_timer);
+
+        this.timerText.setText(Useful.doubleToMinuteSecondString(training.getDuration()));
 
         this.title.setText(training.getName());
         this.description.setText(training.getDescription());
@@ -74,6 +86,34 @@ public class TrainingDetailsFragment extends Fragment {
         int imageResource = resources.getIdentifier(training.getImage(), null, getActivity().getPackageName());
         posters.add(new DrawableImage(imageResource));
         posterSlider.setPosters(posters);
+
+        this.startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!start) {
+                    start = true;
+                    long duration = TimeUnit.MINUTES.toMillis((long) (training.getDuration()  + 1));
+                    startButton.setText("Stop");
+                    new CountDownTimer(duration, 1000) {
+                        @Override
+                        public void onTick(long l) {
+                            String sDuration = String.format(Locale.ENGLISH, "%02d:%02d",
+                                    TimeUnit.MICROSECONDS.toMinutes(l),
+                                    TimeUnit.MILLISECONDS.toSeconds(l) -
+                                            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(l)));
+                            timerText.setText(sDuration);
+                        }
+
+                        @Override
+                        public void onFinish() {
+                            startButton.setText("Go");
+                            Toast.makeText(getContext(), "Félicitation vous l'avez fait", Toast.LENGTH_LONG).show();
+                            start = false;
+                        }
+                    }.start();
+                }
+            }
+        });
 
         return view;
     }
